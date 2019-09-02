2dff0d93f1c5ba1c30e53c91694d80c9779abec9
ajisukmo44 <ajisuk31@gmail.com>
2019-09-02 15:21:21 +0700

    menambahkan file main.js

---
+++ 01-01/main.js
@@ -0,0 +1,18 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Google Chrome is Cool, ' + myName;
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Google Chrome is Cool, ' + storedName;
+}
+
+myButton.onclick = function () {
+        setUserName();
