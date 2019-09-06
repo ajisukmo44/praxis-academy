ccc0ff72a1757e7f8a33182982241341fb9e7550
ajisukmo44 <ajisuk31@gmail.com>
2019-09-06 16:36:35 +0700

    mengubah file kasus 2

---
+++ novice/01-05/kasus/kasus2.js
@@ -0,0 +1,12 @@
+var kata = prompt("masukan kata": )
+
+
+let prices = Object.fromEntries([
+    ['banana', 1],
+    ['orange', 2],
+    ['meat', 4]
+]);
+
+// now prices = { banana: 1, orange: 2, meat: 4 }
+
+alert(prices.orange);
