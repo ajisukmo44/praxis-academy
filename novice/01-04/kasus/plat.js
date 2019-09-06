5a8039efbb1ca2c53d58d8d4376965b64896cc53
ajisukmo44 <ajisuk31@gmail.com>
2019-09-06 15:22:42 +0700

    mengubah  file

--- novice/01-04/kasus/plat.js
+++ novice/01-04/kasus/plat.js
@@ -1,9 +1,8 @@
         var plat = /^([A-Za-z]{1,2})(\s)*([0-9]{1,4})(\s)*([A-Za-z]{0,3})$/i;
-
         function test(noInput) {
             var OK = plat.exec(noInput.value);
             if (!OK)
-                window.alert(' Nomor Kendaraan ' + noInput.value + ' bukan dari Indonesia');
+                window.alert(' Nomor Kendaraan '+ noInput.value + ' bukan dari Indonesia');
             else
                 window.alert('Plat Nomor Kendaraan ' + noInput.value + ' Berasal Dari Indonesia');
-        }
+        
