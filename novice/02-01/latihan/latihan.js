811fca07f55449f2e8b2cb5d56f402be681d44eb
ajisukmo44 <ajisuk31@gmail.com>
2019-09-09 15:07:34 +0700

    menambahkan latihan

---
+++ novice/02-01/latihan/latihan.js
@@ -0,0 +1,31 @@
+Shape = {
+    type: 'Shape',
+    getType: function () {
+        return this.type;
+    }
+}
+
+function Triangle(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.type = 'Triangle';
+}
+
+Triangle.prototype = Shape;
+
+Triangle.prototype.constructor = Triangle;
+
+Triangle.prototype.getPerimeter = function () {
+    return this.a + this.b + this.c;
+
+}
+
+var t = new Triangle(1, 2, 3);
+console.log(t.constructor === Triangle);
+console.log(Shape.isPrototypeOf(t));
+console.log(t.getPerimeter());
+console.log(t.getType());
+
+var myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].shuffle();
+console.log(myArray);
