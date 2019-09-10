1c86b1c8cc569b4b4e7ec9392ee75a43a2326e2e
ajisukmo44 <ajisuk31@gmail.com>
2019-09-10 14:17:32 +0700

    menambahkan file kasus.js

---
+++ novice/02-02/kasus/kasus.js
@@ -0,0 +1,35 @@
+class cat {
+    constructor(name, type, color) {
+        this.name = name;
+        this.type = type;
+        this.color = color;
+        this.eat = 0;
+    }
+
+    cateat() {
+        this.eat++;
+        console.log(this.name, this.type, this.color, 'eat');
+        return this;
+
+    }
+
+    fatigue() {
+        this.fatigue;
+        console.log(this.name, this.type, this.color, 'fatigue');
+        return this;
+
+    }
+
+    sleep() {
+        this.sleep;
+        console.log(this.name, this.type, this.color, 'sleep');
+        return this;
+
+    }
+
+
+}
+var cat1 = new cat('bony', 'persia', 'brown');
+var cat2 = new cat('bona', 'persia', 'black');
+
+cat1.cateat().cateat().fatigue().sleep();
