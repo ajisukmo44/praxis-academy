805b4c3cfd040cdb3a33da28fe87a6ac30dce319
ajisukmo44 <ajisuk31@gmail.com>
2019-09-10 11:40:19 +0700

    edit file

--- novice/02-01/latihan/latihan.js
+++ novice/02-01/latihan/latihan.js
@@ -27,5 +27,22 @@ console.log(Shape.isPrototypeOf(t));
 console.log(t.getPerimeter());
 console.log(t.getType());
 
-var myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].shuffle();
-console.log(myArray);
+Array.prototype.shuffle = function () {
+    var input = this;
+
+    for (var i = input.length - 1; i >= 0; i--) {
+
+        var randomIndex = Math.floor(Math.random() * (i + 1));
+        var itemAtIndex = input[randomIndex];
+
+        input[randomIndex] = input[i];
+        input[i] = itemAtIndex;
+    }
+    return input;
+}
+
+var tempArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+tempArray.shuffle();
+
+
+console.log(tempArray);
