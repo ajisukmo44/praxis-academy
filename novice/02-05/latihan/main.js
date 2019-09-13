dcb387cdc5e1fdcdf192cc5de2e1275e652296ac
ajisukmo44 <ajisuk31@gmail.com>
2019-09-13 15:09:40 +0700

    mengedit file

---
+++ novice/02-05/latihan/main.js
@@ -0,0 +1,11 @@
+//main.js
+let myLogModule = require('./module.js');
+let ktp = require('./moduleKTP.js/index.js');
+
+// myLogModule.info('Node.js started');
+myLogModule.tambah(5);
+
+ktp.nik();
+
+//panggil moduleKTP
+ktp.users();
