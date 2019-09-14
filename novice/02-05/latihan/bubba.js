0e9905af03ec54c589062978a59bf803782a6edc
ajisukmo44 <ajisuk31@gmail.com>
2019-09-14 22:13:38 +0700

    mengedit file

---
+++ novice/02-05/latihan/bubba.js
@@ -0,0 +1,13 @@
+export function myFunc(num) {
+    return num + 1;
+}
+
+function myFunc2(num) {
+    return num * 2;
+}
+
+function myFunc3(num) {
+    return num / 3;
+}
+
+// export { myFunc,    myFunc2 };
