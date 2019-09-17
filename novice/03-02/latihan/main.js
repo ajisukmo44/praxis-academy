2e8d50fe8c04da1199746a13117174e32f39f4ea
ajisukmo44 <ajisuk31@gmail.com>
2019-09-17 15:57:50 +0700

    mengedit file

--- novice/03-02/latihan/main.js
+++ novice/03-02/latihan/main.js
@@ -7,46 +7,46 @@
 //     });
 
 
+// postData('https://jsonplaceholder.typicode.com/users', {
+//         answer: 42
+//     })
+//     .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+//     .catch(error => console.error(error));
+
+// function postData(url = '', data = {}) {
+//     // Default options are marked with *
+//     return fetch(url, {
+//             method: 'POST', // *GET, POST, PUT, DELETE, etc.
+//             mode: 'cors', // no-cors, *cors, same-origin
+//             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+//             credentials: 'same-origin', // include, *same-origin, omit
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 // 'Content-Type': 'application/x-www-form-urlencoded',
+//             },
+//             redirect: 'follow', // manual, *follow, error
+//             referrer: 'no-referrer', // no-referrer, *client
+//             body: JSON.stringify(data), // body data type must match "Content-Type" header
+//         })
+//         .then(response => response.json()); // parses JSON response into native JavaScript objects 
+// }
+
+
+
 // Uploading JSON data
 
-// var url = 'file:///C:/Users/Acer%20E%2014/Desktop/praxis-academy/novice/03-02/latihan/index.html';
-// var data = {
-//     username: 'joni',
-//     password: '123'
-// };
+var url = 'https://jsonplaceholder.typicode.com/users';
+var data = {
+    username: 'joni',
+    password: '123'
+};
 
-// fetch(url, {
-//         method: 'POST', // or 'PUT'
-//         body: JSON.stringify(data), // data can be `string` or {object}!
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     }).then(res => res.json())
-//     .then(response => console.log('Success:', JSON.stringify(response)))
-//     .catch(error => console.error('Error:', error));
-
-
-
-postData('https://jsonplaceholder.typicode.com/users', {
-        answer: 42
-    })
-    .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
-    .catch(error => console.error(error));
-
-function postData(url = '', data = {}) {
-    // Default options are marked with *
-    return fetch(url, {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, *cors, same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
-            headers: {
-                'Content-Type': 'application/json',
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            redirect: 'follow', // manual, *follow, error
-            referrer: 'no-referrer', // no-referrer, *client
-            body: JSON.stringify(data), // body data type must match "Content-Type" header
-        })
-        .then(response => response.json()); // parses JSON response into native JavaScript objects 
-}
+fetch(url, {
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(error => console.error('Error:', error));
